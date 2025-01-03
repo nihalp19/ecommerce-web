893d53209a820d19986bc19a24077480168187f8
nihalp19 <nihalpanday2020@gmail.com>
2025-01-03 13:23:01 +0530

    configured redis

--- backend/controllers/auth-controllers.js
+++ backend/controllers/auth-controllers.js
@@ -1,4 +1,41 @@
 import User from "../models/user-models.js"
+import jwt from "jsonwebtoken"
+import {redis} from "../db/redis.js"
+import dotenv from "dotenv"
+dotenv.config()
+
+const generateToken = (res,userId) => {
+    const accessToken = jwt.sign({userId},process.env.SECERET_KEY_ACCESSTOKEN,{
+        expiresIn : '15m'
+    }) 
+
+    const refreshToken = jwt.sign({userId},process.env.SECRET_KEY_REFRESHTOKEN,{
+        expiresIn : '7d'
+    })
+
+    return {accessToken,refreshToken}
+}
+
+const  storeRefreshToken= async(userId,refreshToken) => {
+    await redis.set(`refresh-token${userId}`,refreshToken,"EX",7*24*60*60)
+}
+
+const setCookies = (res,accessToken,refreshToken) => {
+    res.cookie("accessToken",accessToken,{
+        httpOnly : true,
+        secure : process.env.NODE_ENV === "production",
+        sameSite : "strict",
+        maxAge: 15 * 60 * 1000,
+    })
+    res.cookie("refreshToken",refreshToken,{
+        httpOnly : true,
+        secure : process.env.NODE_ENV === "production",
+        sameSite : "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+    })
+}
+
+
 export const signup = async(req,res) => {
     try{
         const {name,email,password} = req.body
@@ -9,6 +46,11 @@ export const signup = async(req,res) => {
         }
 
         const user = await User.create({name,email,password})
+        const {accessToken,refreshToken} = generateToken(user._id)
+        await storeRefreshToken(user._id,refreshToken)
+
+       setCookies(res,accessToken,refreshToken)
+
         return res.status(200).json({succes : true,message : "User registered successfully",user : {
             ...user._doc,
             password : undefined
